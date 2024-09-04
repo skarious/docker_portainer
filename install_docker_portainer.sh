6f0a29eb9e6f9213382ea891c7bb04ca63381304
skarious <xxskariousxx@gmail.com>
2024-09-04 10:27:58 -0400

    Update install_docker_portainer.sh
    MAS controles si estan instalados no hacer nada

--- install_docker_portainer.sh
+++ install_docker_portainer.sh
@@ -20,6 +20,11 @@ check_portainer() {
     fi
 }
 
+# Función para obtener la IP local
+get_ip() {
+    hostname -I | awk '{print $1}'
+}
+
 # Actualizar el sistema
 sudo apt-get update
 sudo apt-get upgrade -y
@@ -42,7 +47,7 @@ if ! check_docker; then
     # Añadir el usuario actual al grupo docker (opcional)
     sudo usermod -aG docker $USER
 else
-    echo -e "\e[32mLa instalación de Docker ha sido omitida.\e[0m"
+    echo -e "\e[32mDocker ya está instalado y en ejecución.\e[0m"
 fi
 
 # Descargar e instalar Portainer si no está instalado
@@ -54,12 +59,14 @@ if ! check_portainer; then
       -v portainer_data:/data \
       portainer/portainer-ce
 else
-    echo -e "\e[32mLa instalación de Portainer ha sido omitida.\e[0m"
+    IP=$(get_ip)
+    echo -e "\e[32mPortainer ya está instalado y corriendo en los siguientes puertos e IP:"
+    echo -e "http://$IP:9000\e[0m"
 fi
 
 # Mostrar información de acceso en color verde
-echo -e "\e[32mDocker y Portainer han sido instalados y configurados."
-echo -e "Puedes acceder a Portainer a través de la siguiente URL: http://$(hostname -I | awk '{print $1}'):9000\e[0m"
+echo -e "\e[32mLa instalación ha sido completada (si no estaba previamente instalada)."
+echo -e "Puedes acceder a Portainer a través de la siguiente URL: http://$(get_ip):9000\e[0m"
 
 # Dibujar el nombre Ronald con barras, puntos y comas en verde
 echo -e "\e[32m"
